13c8fc7ee2e98641d22dad3c12e43ae2018ea887
amalia-ullman <mali.ullman@gmail.com>
2025-12-05 08:53:53 -0800

    limelight 🟢🟢🟢

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -27,12 +27,15 @@ public class RobotContainer {
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
             .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    private final SwerveRequest.RobotCentric driveRobotOriented = new SwerveRequest.RobotCentric()
+            .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
+            .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
-    private final CommandXboxController joystick = new CommandXboxController(0);
+    private final CommandXboxController controller = new CommandXboxController(0);
 
     public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
@@ -40,17 +43,54 @@ public class RobotContainer {
         configureBindings();
     }
 
+    double kP_angle = 0.2;
+    double currentTA = 0;
+    double currentTX = 0;
+
+    public double LimelightTranslation(double ta) {
+        double translation = 0;
+        if (ta <= 2.0) {
+            translation = 2.5;     
+        } else if (ta > 2.0 && ta < 8.0) {
+            translation = 1.5 * Math.sqrt(8.0 - ta) * 0.4;
+        } else if (ta >= 8.0 && ta < 15.0) {
+            translation = 0.5;
+        } else {
+            translation = 0;
+        }
+        return translation;
+    }
+    
     private void configureBindings() {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-            )
+            drivetrain.applyRequest(() -> {
+                double vx = 0;
+                double vy = 0;
+                double angle = 0;
+                
+                currentTA = LimelightHelpers.getTA("limelight");
+                currentTX = LimelightHelpers.getTX("limelight");             
+                
+                if (controller.a().getAsBoolean()) {                 
+                    vx = 0;
+                    vy = LimelightTranslation(currentTA) * -1;
+                    angle = currentTX * kP_angle * -1;
+                    return driveRobotOriented.withVelocityX(0)
+                                .withVelocityY(0)
+                                .withRotationalRate(Math.toRadians(angle));
+                } else {
+                    vx = (-controller.getLeftY() * MaxSpeed) * 0.5;
+                    vy = (-controller.getLeftX() * MaxSpeed) * 0.5;
+                    angle = (-controller.getRightX() * MaxAngularRate);
+                return drive.withVelocityX(vy)
+                            .withVelocityY(vx)
+                            .withRotationalRate((angle));
+                }
+            })
         );
+    
 
         // Idle while the robot is disabled. This ensures the configured
         // neutral mode is applied to the drive motors while disabled.
@@ -59,20 +99,20 @@ public class RobotContainer {
             drivetrain.applyRequest(() -> idle).ignoringDisable(true)
         );
 
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
+        //controller.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        controller.b().whileTrue(drivetrain.applyRequest(() ->
+            point.withModuleDirection(new Rotation2d(-controller.getLeftY(), -controller.getLeftX()))
         ));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
-        joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+        controller.back().and(controller.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+        controller.back().and(controller.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+        controller.start().and(controller.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+        controller.start().and(controller.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
         // reset the field-centric heading on left bumper press
-        joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
+        controller.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
 
         drivetrain.registerTelemetry(logger::telemeterize);
     }
