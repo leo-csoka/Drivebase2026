a7640cf138bd70c5d2209caa1659e36f697a7ced
warroboticss <wtechrobotics@gmail.com>
2025-12-17 10:40:24 -0800

    doesnt work lol!!! :p :> :(((((

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -176,6 +176,6 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        return new PathPlannerAuto("example");
+        return new PathPlannerAuto("Example");
     }
 }
